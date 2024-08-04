e8383f8d01263247f56cdd4e113e4d27566f4106
kaspernj <k@spernj.org>
2024-08-04 17:20:38 +0000

    Share global render state

--- src/shared.js
+++ src/shared.js
@@ -1,6 +1,10 @@
-const shared = {
-  rendering: 0,
-  renderingCallbacks: []
+if (!window.setStateCompareData) {
+  window.setStateCompareData = {
+    rendering: 0,
+    renderingCallbacks: []
+  }
 }
+
+const shared = window.setStateCompareData
 
 export default shared
